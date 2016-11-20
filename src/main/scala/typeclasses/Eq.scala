d9faa4908ba210f7e467a79e0b4b23221cf95f5f
Abdul Sattar <asattar@exchangesolutions.com>
2016-11-20 12:54:56 -0500

    Type classes for polymorphic data types in scala

--- src/main/scala/typeclasses/Eq.scala
+++ src/main/scala/typeclasses/Eq.scala
@@ -7,13 +7,14 @@ trait Eq[T]{
 
 
 sealed trait Tree[+A]
+final case class Empty() extends  Tree[Nothing]
 final case class Node[A](value: A) extends Tree[A]
 final case class Branch[A](value: A, left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree{
 
-  implicit def eqTree[T, A[T] <: Tree[T]]: Eq[A[T]] = new Eq[A[T]] {
-    override def ==(t1: A[T], t2: A[T]): Boolean = true
+  implicit def eqTree[T]: Eq[Tree[T]] = new Eq[Tree[T]] {
+    override def ==(t1: Tree[T], t2: Tree[T]): Boolean = true
   }
 }
 
@@ -30,9 +31,9 @@ object App1 extends App{
 
   import Tree._
 
-  def equality[T](t1: T, t2: T)(implicit eq: Eq[T]): Boolean = eq.==(t1, t2)
+  def equality[T: Eq](t1: T, t2: T): Boolean = implicitly[Eq[T]].==(t1, t2)
 
-  println(equality(Node(1), Node(2)))
+  println(equality[Tree[Int]](Empty(), Node(2)))
 
-  println(equality(MyClass(), MyClass()))
+  //eqTree[Int].==(Empty(), Node(2))
 }
